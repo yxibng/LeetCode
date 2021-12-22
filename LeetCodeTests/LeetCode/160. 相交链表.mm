a127070f140aa985f7defdb62d53439d9acb0e6a
yaoxiaobing <xiaobing.yao@duobei.com>
2021-12-22 23:05:45 +0800

    feat: 160. 相交链表

---
+++ LeetCodeTests/LeetCode/160. 相交链表.mm
@@ -0,0 +1,56 @@
+//
+//  Test202112222300.m
+//  LeetCodeTests
+//
+//  Created by yxibng on 2021/12/22.
+//
+
+#import <XCTest/XCTest.h>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ 
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *pA = headA;
+        ListNode *pB = headB;
+        while (pA != pB) {
+            pA = pA == nullptr ? headB : pA->next;
+            pB = pB == nullptr ? headA : pB->next;
+        }
+        return pA;
+    }
+};
+
+@interface Test202112222300 : XCTestCase
+
+@end
+
+@implementation Test202112222300
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
